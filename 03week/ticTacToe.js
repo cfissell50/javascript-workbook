0cc1698024299410a6079a8a8ecc1efcc7e89cd8
cfissell50 <cfissell@hotmail.com>
2017-10-24 17:05:19 -0500

    moar stuff

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -30,10 +30,7 @@ const horizontalWin = () => {
   }
 }
 
-/* Run an conditional statement to make columns all equal to the same string
-In columns "0", "1", "2" make variable 'block' for function verticalWin equal:
-Column "0" =>[0,0], [1,0], [2,0] and Column "1" => [0,1], [1,1], ]2,0}
-Column "2" =>[0,2], [1,2],[2,2]. */
+
 const verticalWin = () => {
   if ((board[0][0] === board[1][0] && board[1][0] === board[2][0]) || (board[0][1] === board[1][1] && board[1][1] === board[2][1]) || (board[0][2] === board[1][2] &&
       board[1][2] === board[2][2])) {
@@ -81,6 +78,7 @@ function ticTacToe(row, column) {
   // } else {
     // console.log('Please enter a valid index.  Valid values are 0, 1, 2');
 }
+
 
 
 function getPrompt() {
