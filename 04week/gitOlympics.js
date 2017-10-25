d632daa171f3d6978085344e23afa738b316e94a
cfissell50 <cfissell@hotmail.com>
2017-10-24 19:05:29 -0500

    almost there

--- 04week/gitOlympics.js
+++ 04week/gitOlympics.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let arr1 = ['Zeus', 'Hades', 'Poseidon', 'Apollo', 'Hera'];
+const arr1 = ['Zeus', 'Hades', 'Poseidon', 'Apollo', 'Hera'];
 
   const printListOfOlympians = (arr1) => {
     arr1.forEach(olympians) => {
