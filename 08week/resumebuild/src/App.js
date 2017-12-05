8001ea46843c42d197131f83c696156c71d131e6
cfissell50 <cfissell@hotmail.com>
2017-12-05 13:00:19 -0600

    lets keep going

--- 08week/resumebuild/src/App.js
+++ 08week/resumebuild/src/App.js
@@ -3,19 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+ state = {
+     characters: null
+ }
+ componentDidMount(){
+    const that = this;
+    fetch('https://swapi.co/api/people/')
+        .then((response) => response.json())
+        .then((responseJson) => {
+            that.setState({characters: responseJson.results})
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+ }
+ renderCharacters(){
+     const { characters } = this.state;
+     if(characters && characters.length > 0){
+         return characters.map((obj, key) =>{
+             return <p key={key}>{obj.name}</p>
+         })
+     }
+ }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h2 className="App-title">Star Wars</h2>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          {this.renderCharacters()}
       </div>
     );
   }
 }
-hey
+
 export default App;
